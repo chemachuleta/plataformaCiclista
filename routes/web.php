e9cc966d8fce8b481c369c6f093e0d26f7f5d51c
chema <chemachuletilla@gmail.com>
2026-02-11 11:37:22 +0100

    Inclusion de ruta /

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 
 Auth::routes();
 
+Route::get('/', 'HomeController@index')->name('root');
 Route::get('/home', 'HomeController@index')->name('home');
